2798c9407a0deabf9122248bf8d9a1f44c2a078d
Felipe Ferreira <felipe.ferreira.ads@gmail.com>
2020-08-26 22:12:41 -0300

    fixes

--- wp-content/themes/my-niver/source/js/components/partners.js
+++ wp-content/themes/my-niver/source/js/components/partners.js
@@ -1,22 +1,17 @@
 function getPartners () {
-    $.ajax({
-        url: 'https://app.myniver.net/estabsniver',
-        crossDomain: true,
-        dataType: 'json',
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-        }
-    }).done(response => {
-        const template = Object.values(response).map(row => `
-            <div class="card-partner">
-                <figure>
-                    <img src="https://myniver.net/images/logos/${row.logo}" alt="${row.name}">
-                </figure>
-                <h3>${row.name}</h3>
-            </div>
-        `);
-        $('#carousel-partners').append(template);
-    });
+    fetch('https://app.myniver.net/estabsniver', { method: 'GET', mode: 'no-cors' })
+        .then(response => response.json())
+        .then(response => {
+            const template = Object.values(response).map(row => `
+                <div class="card-partner">
+                    <figure>
+                        <img src="https://app.myniver.net/images/logos/${row.logo}" alt="${row.name}">
+                    </figure>
+                    <h3>${row.name}</h3>
+                </div>
+            `);
+            $('#carousel-partners').append(template);
+        });
 }
 
 getPartners();
